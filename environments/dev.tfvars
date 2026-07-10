98eac458e7009e030e014757d0e84e843776fc47
Gokuleeswaran22 <gokuleeswarang@gmail.com>
2026-07-10 09:20:41 +0530

    Add network module with virtual network and subnet

---
+++ environments/dev.tfvars
@@ -0,0 +1,14 @@
+resource_group_name = "rg-dev-demo"
+location            = "Central India"
+
+vnet_name = "vnet-dev"
+
+vnet_address_space = [
+  "10.0.0.0/16"
+]
+
+subnet_name = "subnet-dev"
+
+subnet_prefixes = [
+  "10.0.1.0/24"
+]
---
+++ main.tf
@@ -0,0 +1,19 @@
+module "resource_group" {
+  source = "./modules/resource-group"
+
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
+
+module "network" {
+  source = "./modules/network"
+
+  resource_group_name = module.resource_group.resource_group_name
+  location            = var.location
+
+  vnet_name          = var.vnet_name
+  vnet_address_space = var.vnet_address_space
+
+  subnet_name     = var.subnet_name
+  subnet_prefixes = var.subnet_prefixes
+}
